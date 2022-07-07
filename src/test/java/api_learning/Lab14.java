3a509c644318687eeae67cdec8261fce0f136e3d
Savills C6JGV93 <briceto102@gmail.com>
2022-07-07 09:38:21 +0700

    maven appium commit

---
+++ src/test/java/api_learning/Lab14.java
@@ -0,0 +1,32 @@
+package api_learning;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class Lab14 {
+
+    public static void main(String[] args) {
+        AppiumDriver<MobileElement> appiumDriver = null;
+
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName","Android");
+        desiredCapabilities.setCapability("automationmName","uiautomator2");
+        desiredCapabilities.setCapability("udid","LMV409N0831cfe7");
+        desiredCapabilities.setCapability("appPackage","com.wdiodemoapp");
+        desiredCapabilities.setCapability("appActivity","com.wdiodemoapp.MainActivity");
+
+       try {
+           URL appiumServer = new URL("http://localhost:4723/wd/hub");
+           appiumDriver = new AndroidDriver<MobileElement>(appiumServer, desiredCapabilities);
+//           Thread.sleep(3000);
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+        appiumDriver.quit();
+
+    }
+}
