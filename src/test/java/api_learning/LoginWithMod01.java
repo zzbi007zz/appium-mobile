00ad4074ee2ca9651e60d27780f0ca23c76ad372
Savills C6JGV93 <briceto102@gmail.com>
2022-07-26 19:14:39 +0700

    update

--- src/test/java/api_learning/LoginWithMod01.java
+++ src/test/java/api_learning/LoginWithMod01.java
@@ -2,7 +2,9 @@ package api_learning;
 
 import driver.DriverFactory;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
+import models.pages.LoginScreenMod01;
 import platform.Platform;
 
 public class LoginWithMod01 {
@@ -11,7 +13,13 @@ public class LoginWithMod01 {
     public static void main(String[] args) {
         AppiumDriver<MobileElement> appiumDriver = DriverFactory.getDriver(Platform.ANDROID);
         try {
+            MobileElement navLoginScreen = appiumDriver.findElement(MobileBy.AccessibilityId("Login"));
+            navLoginScreen.click();
 
+            LoginScreenMod01 loginScreenMod01 = new LoginScreenMod01(appiumDriver);
+            loginScreenMod01.usernameSel().sendKeys("test@mail.com");
+            loginScreenMod01.passwordSel().sendKeys("12345678");
+            loginScreenMod01.loginBtnSel().click();
 
         } catch(Exception e)
         {e.printStackTrace();
--- src/test/java/models/pages/LoginScreenMod01.java
+++ src/test/java/models/pages/LoginScreenMod01.java
@@ -6,7 +6,6 @@ import io.appium.java_client.MobileElement;
 import org.openqa.selenium.By;
 
 public class LoginScreenMod01 {
-
     private final AppiumDriver<MobileElement> appiumDriver;
     private final static By usernameSel= MobileBy.AccessibilityId("input-email");
     private final static By passwordSel= MobileBy.AccessibilityId("input-password");
